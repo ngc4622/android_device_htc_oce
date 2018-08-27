c7aad27e76b12bbf28fa270e46948cae5a501002
Tobias Tefke <tobias.tefke@tutanota.com>
2018-08-27 12:15:38 +0200

    Add initial ramdisk
    * also cleanup some files that are no longer needed
    
    Change-Id: I8ecdea4c657ac4e2a9a5a2616c68a691a03f86d8

--- device.mk
+++ device.mk
@@ -13,3 +13,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/root/00-memory-dump:root/crashdump.d/00-memory-dump \
+    $(LOCAL_PATH)/root/cwkeys:root/cwkeys \
+    $(LOCAL_PATH)/root/fstab.qcom:root/fstab.qcom \
+    $(LOCAL_PATH)/root/init.hosd_fusion.usb.rc:root/init.hosd_fusion.usb.rc \
+    $(LOCAL_PATH)/root/init.hosd.htc_oce.rc:root/init.hosd.htc_oce.rc \
+    $(LOCAL_PATH)/root/init.hosd.nanohub.rc:root/init.hosd.nanohub.rc \
+    $(LOCAL_PATH)/root/init.hosd.usb.configfs.rc:root/init.hosd.usb.configfs.rc \
+    $(LOCAL_PATH)/root/init.hosd.usb.rc:root/init.hosd.usb.rc \
+    $(LOCAL_PATH)/root/init.htc.storage.exfat.rc:root/init.htc.storage.exfat.rc \
+    $(LOCAL_PATH)/root/init.htc.storage.mocana.rc:root/init.htc.storage.mocana.rc \
+    $(LOCAL_PATH)/root/init.htc.touch.rc:root/init.htc.touch.rc \
+    $(LOCAL_PATH)/root/init.recovery.common.rc:root/init.recovery.common.rc \
+    $(LOCAL_PATH)/root/init.recovery.hlthchrg.rc:root/init.recovery.hlthchrg.rc \
+    $(LOCAL_PATH)/root/init.recovery.htc_oce.rc:root/init.recovery.htc_oce.rc \
+    $(LOCAL_PATH)/root/init.recovery.service.rc:root/init.recovery.service.rc \
+    $(LOCAL_PATH)/root/ueventd.htc_oce.rc:root/ueventd.htc_oce.rc
