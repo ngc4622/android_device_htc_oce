9362ac6985e8c36615afc98a95d29b3db94a8fab
Tobias Tefke <tobias.tefke@tutanota.com>
2018-09-26 11:09:27 +0200

    Add common qcom proprietary blobs

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,7 @@ fi
 # Initialize the helper
 setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
+extract "$MY_DIR"/proprietary-files-qc.txt "$SRC"
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
 "$MY_DIR"/setup-makefiles.sh
